991fde6053e1d41e0f63b4579cd901f4dfe49b22
Logan Rothe <101854144+Logan-Rothe@users.noreply.github.com>
2026-02-13 11:46:00 -0800

    change to console logging

--- app/recruitment/case-practice/page.tsx
+++ app/recruitment/case-practice/page.tsx
@@ -1,9 +1,16 @@
 import {getCaseData} from './static';
 import {CaseData} from './static/types';
+import { useEffect } from "react";
 //This page contains all of the cases
 //TODO: Build case selection component
 export default function CaseLandingPage(){
-    //function stuff
+    
+    useEffect(() => {
+        const practice: Record<string, CaseData> = getCaseData();
+        console.log("something happens");
+        console.log(practice);
+    }, []);
+
     const practice : Record<string, CaseData> = getCaseData();
     console.log("something happens");
     console.log(practice);
