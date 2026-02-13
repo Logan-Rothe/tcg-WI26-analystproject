884c2388eaa821737465490ba07cdc12d16f1e3f
Logan Rothe <101854144+Logan-Rothe@users.noreply.github.com>
2026-02-13 11:26:50 -0800

    fix to misrouting error

--- app/recruitment/case-practice/page.tsx
+++ app/recruitment/case-practice/page.tsx
@@ -1,5 +1,5 @@
-import {getCaseData} from '@/app/recruitment/case-practice/static/index.js';
-import {CaseData} from '@/app/recruitment/case-practice/static/types.js';
+import {getCaseData} from './static';
+import {CaseData} from './static/types';
 //This page contains all of the cases
 //TODO: Build case selection component
 export default function CaseLandingPage(){
