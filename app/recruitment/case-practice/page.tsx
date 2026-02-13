9de655db2ff3efeb06000f71c43d34850bd6e542
Logan Rothe <101854144+Logan-Rothe@users.noreply.github.com>
2026-02-13 11:24:39 -0800

    dummy forgot backslash. dummy added backslash

--- app/recruitment/case-practice/page.tsx
+++ app/recruitment/case-practice/page.tsx
@@ -1,5 +1,5 @@
-import {getCaseData} from '@app/recruitment/case-practice/static/index.js';
-import {CaseData} from '@app/recruitment/case-practice/static/types.js';
+import {getCaseData} from '@/app/recruitment/case-practice/static/index.js';
+import {CaseData} from '@/app/recruitment/case-practice/static/types.js';
 //This page contains all of the cases
 //TODO: Build case selection component
 export default function CaseLandingPage(){
