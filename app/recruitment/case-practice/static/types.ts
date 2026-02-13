9948edffe79fdff032fb226614c73f8f6a0440ed
Logan Rothe <101854144+Logan-Rothe@users.noreply.github.com>
2026-02-13 11:07:57 -0800

    Added json data handling and laided out general file structure

---
+++ app/recruitment/case-practice/static/types.ts
@@ -0,0 +1,37 @@
+//DEFINE ALL THE TYPES FOR ITEMS IN JSON
+
+//Generalized data of case
+export interface CaseData {
+  name: string;
+  time: number;
+  sections: Section[];
+}
+
+//Each modular section
+export interface Section {
+  name: string;
+  time: number;
+  type: 'writing' | 'brainstorm' | 'framework';
+  content: {
+    prompt: string;
+    images: Image[];
+    hint?: string;
+  };
+  answer: Answer;
+}
+
+//Image component
+export interface Image {
+  url: string;
+  width: number;
+  height: number;
+  alt?: string;
+}
+
+//Answer component
+export interface Answer {
+  showAnswer: boolean;
+  type: 'text' | 'table';
+  headers?: string[];
+  rows?: string[][];
+}
