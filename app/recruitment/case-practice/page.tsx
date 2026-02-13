8bbd72983a105f5ce0575977d264dc8efdc1e1a2
Logan Rothe <101854144+Logan-Rothe@users.noreply.github.com>
2026-02-13 11:20:39 -0800

    connected static files to landing page

--- app/recruitment/case-practice/page.tsx
+++ app/recruitment/case-practice/page.tsx
@@ -1,8 +1,12 @@
- 
-
-export default function CasePracticePage(){
+import {getCaseData} from '@app/recruitment/case-practice/static/index.js';
+import {CaseData} from '@app/recruitment/case-practice/static/types.js';
+//This page contains all of the cases
+//TODO: Build case selection component
+export default function CaseLandingPage(){
     //function stuff
-
+    const practice : Record<string, CaseData> = getCaseData();
+    console.log("something happens");
+    console.log(practice);
     return(
         <div>
             Welcome to the Practice Page
